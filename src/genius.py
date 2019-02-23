78a7e20a405f9703b3397ed24d3f21121408d750
Dennis Pettersson <dennis.pettersson@iteam.se>
2019-02-23 12:40:17 +0100

    chore: more filters

--- src/genius.py
+++ src/genius.py
@@ -8,26 +8,25 @@ genius = lyricsgenius.Genius(config.access_token)
 genius.verbose = config.verbose
 genius.skip_non_songs = True
 genius.remove_section_headers = True
-genius.excluded_terms = ["(Remix)", "(Live)"]
+genius.excluded_terms = [
+    "(Remix)",
+    "(Live)",
+    "(Akustisk)",
+    "(Instrumental)",
+    "(Radio Edit)",
+]
 
 
 def search(artist):
     artists = set()
 
-    results = genius.search_artist(
-        artist,
-        max_songs=config.max_songs,
-    )
+    results = genius.search_artist(artist, max_songs=config.max_songs)
 
     for song in results.songs:
         if not song.album:  # ignore ep/singles
             continue
 
-        file_path = "{}/{}/{}.txt".format(
-            song.artist,
-            song.album,
-            song.title,
-        )
+        file_path = "{}/{}/{}.txt".format(song.artist, song.album, song.title)
 
         set_content(file_path, content=song.lyrics)
         artists.add(song.artist)
